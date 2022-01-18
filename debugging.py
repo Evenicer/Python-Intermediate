6a2a37e40c49a16ab3f840958ddd4c92b3262b05
Rogelio Valle <evenicervalle@gmail.com>
2022-01-18 12:20:43 -0600

    Debugging

--- debugging.py
+++ debugging.py
@@ -1,18 +1,20 @@
-def divisors(num):
-    divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors        
+def divisor(num):
+    try:
+        if num < 0:
+            raise ValueError("ingresa un número positvo")
+        divisors =[i for i in range(1, num + 1) if num % i == 0]
+        return divisors
+    except ValueError as ve:
+        print(ve)
 
 
 def run():
-    try:
-        num = int(input("Ingresa un numero: "))
-        print(divisors(num))
-        print("Termino mi programa")
+    try: 
+        num = int(input(f'Ingresa un número: '))      
+        print(divisor(num))
+        print("termino mi programa")
     except ValueError:
-        print("Debes ingresar un numero")    
-
-if __name__ == '__main__':
+        print("ingresa un número")
+    
+if __name__ =='__main__':
     run()
